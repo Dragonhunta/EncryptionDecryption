f26ac2205df80e27aed1a938028b7463a030febe
Anica <154228502+Dragonhunta@users.noreply.github.com>
2024-09-06 17:52:06 +0200

    Add files via upload

---
+++ polyalphabeticalDecryption.py
@@ -0,0 +1,73 @@
+# decrypt text
+# uncomment commented lines for debugging
+
+from math import ceil
+
+# text = "ufz" # says "ufo"
+# in_keyword = "aal"
+
+text = input("Enter an encrypted text: ").lower()
+in_keyword = input("Enter the keyword: ").lower()
+
+decrypt_table = [
+    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    ["b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a"],
+    ["c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b"],
+    ["d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c"],
+    ["e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d"],
+    ["f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e"],
+    ["g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f"],
+    ["h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g"],
+    ["i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h"],
+    ["j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i"],
+    ["k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j"],
+    ["l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k"],
+    ["m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l"],
+    ["n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m"],
+    ["o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n"],
+    ["p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"],
+    ["q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"],
+    ["r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"],
+    ["s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r"],
+    ["t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s"],
+    ["u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t"],
+    ["v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u"],
+    ["w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v"],
+    ["x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w"],
+    ["y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x"],
+    ["z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"],
+
+]
+
+keyword_index = 0
+keyword = in_keyword * (ceil(len(text) / len(in_keyword)))
+# print(keyword)
+
+translated_text = ""
+
+for i in range(len(text)):
+
+    # print()
+
+    # add a space in the text and go to the next keyword letter
+    if text[i] == " ":
+        translated_text += text[i]
+        keyword_index += 1
+        # print("space")
+    # add a symbol in the text and don't go to the next keyword letter
+    elif text[i] == "," or text[i] == "." or text[i] == "!" or text[i] == "?":
+        translated_text += text[i]
+        # print("symbol")
+    else:
+        # find the translated letter in the decrypt table
+        y = decrypt_table[0].index(keyword[keyword_index])
+        x = decrypt_table[y].index(text[i])
+        translation = decrypt_table[0][x]
+        translated_text += translation
+        keyword_index += 1
+        # print(x, y, translation)
+
+
+
+print()
+print(translated_text)
